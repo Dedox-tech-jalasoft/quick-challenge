237b6ef678c01a13b9b6e269b56e09250eb60c99
Diego Murillo <Diego.Murillo@jalasoft.com>
2023-06-08 15:34:11 -0500

    Create first version of the challenge

--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private readonly httpClient: HttpClient) {}
+
+  getUserData(): Observable<any> {
+    return this.httpClient.get<any>("https://randomuser.me/api/?results=20");
+  }
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../core/services/users.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  constructor(private readonly userService: UsersService) {}
+
+  userLocationData: any[] | null = null;
+
+  ngOnInit(): void {
+    this.userService.getUserData().subscribe((data) => {
+      this.userLocationData = data.results.map((user: any) => user.location)
+      console.log(this.userLocationData);
+    });
+  }
 
 }
